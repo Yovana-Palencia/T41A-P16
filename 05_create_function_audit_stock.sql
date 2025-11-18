405cc2d4401cc827669ccbdbf1c06ee93ae6822d
Yovana-Palencia <174197@upslp.edu.mx>
2025-11-18 00:14:46 -0600

    Delete 05_create_function_audit_stock.sql

--- 05_create_function_audit_stock.sql
+++
@@ -1,9 +0,0 @@
--- Función para registrar auditoría de cambios en stock
-CREATE OR REPLACE FUNCTION registrar_auditoria_stock()
-RETURNS TRIGGER AS $$
-BEGIN
-    INSERT INTO auditoria_stock (producto_id, stock_anterior, stock_nuevo)
-    VALUES (OLD.id, OLD.stock, NEW.stock);
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
